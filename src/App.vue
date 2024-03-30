e17dc185dc7f694eaaeed2a484f29d6a57194130
Brian Makumi <bmakumi2000@gmail.com>
2024-03-30 15:11:03 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <UserProfile/>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script> 
+
+import UserProfile from "./components/UserProfile.vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    UserProfile
   }
 }
 </script>
@@ -19,8 +21,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="user-profile">
+      <div class="user-panel">
+          <h1 class="user-name">@ {{  user.username }}</h1>
+
+          <div class="admin-badge" v-if="user.isAdmin">
+              Admin
+          </div>
+          <!-- <h1 class="full-name"> {{ fullName }}</h1> -->
+
+          <div class="follow-count">
+              <strong>Followers</strong> : {{ followers }}
+          </div>
+
+          <!-- <button @click="followUser">
+              Follow
+          </button> -->
+      </div>
+
+      <div class="user-tweets">
+        <div class="user-tweet" v-for="tweet in user.tweets" :key="tweet.id">
+          {{ tweet.content }}
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script> 
+  
+  export default {
+    name: 'App',
+    data(){
+      return{
+        followers: 0,
+        user: {
+          id: 1,
+          username: "uixmakumi",
+          firstName: "Brian",
+          lastName: "Makumi",
+          email: "bmakumi2000@gmail.com",
+          isAdmin: true,
+          tweets:[
+            {id: 1, content: 'Hello World!'},
+            {id: 2, content: 'Trying Twitter For the first time'}
+          ]
+        }
+      }
+    },
+    watch: {
+      followers(newFollowerCount, oldFollowerCount){
+        if(oldFollowerCount < newFollowerCount){
+          console.log(`${this.user.username} has gained a follower!`)
+        }
+      }
+    },
+    computed:{
+      fullName() {
+        return `${this.user.firstName} ${this.user.lastName}`;
+      }
+    },
+    methods:{
+      followUser(){
+        this.followers++;
+      }
+    },
+    mounted(){
+      this.followUser();
+    }
+  }
+  </script>
+  
+  <style>
+    .user-profile{
+      margin-top: 40px;
+    }
+    .user-panel{
+      width: 200px;
+      background: #f2f2f2;
+      padding: 20px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+    }
+    .user-panel .user-name{
+      font-weight: 500;
+      font-size: 1rem;
+    }
+    .user-panel .admin-badge{
+      background-color: red;
+      color: white;
+      border-radius: 5px;
+      margin-right: auto;
+      font-size: 10px;
+      font-weight: 600;
+      padding: 5px 2%;        
+    }
+  </style>
+  
